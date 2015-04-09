69c57b79622c5c3d8ca57d0be89e6e9a3007d608
Piotr Migdal <pmigdal@gmail.com>
2015-04-09 22:10:55 +0200

    microcleaning

--- site/main.js
+++ site/main.js
@@ -1,74 +1,52 @@
 var width = 600,
     height = 500;
 
-// We create a color scale.
 var color = d3.scale.category20();
 
-// We create a force-directed dynamic graph layout.
 var force = d3.layout.force()
     .charge(-100)
     .linkDistance(60)
     .gravity(0.3)
     .size([width, height]);
 
-// In the <div> element, we create a <svg> graphic
-// that will contain our interactive visualization.
+
 var svg = d3.select("#d3graph").append("svg")
   .attr("width", width)
   .attr("height", height);
     
-// We load the JSON file.
+
 d3.json("graph.json", function(error, graph) {
-    // In this block, the file has been loaded
-    // and the 'graph' object contains our graph.
-    
-    // We load the nodes and links in the force-directed
-    // graph.
+
     force.nodes(graph.nodes)
         .links(graph.links)
         .linkStrength(function (d) {
-        // it needs to be here, after links
             return Math.pow(d.weight, 4);
         })
         .start();
 
-    // We create a <line> SVG element for each link
-    // in the graph.
     var link = svg.selectAll(".link")
         .data(graph.links)
         .enter().append("line")
         .attr("class", "link");
 
-    // We create a <circle> SVG element for each node
-    // in the graph, and we specify a few attributes.
     var node = svg.selectAll(".node")
         .data(graph.nodes)
         .enter().append("circle")
         .attr("class", "node")
-        .attr("r", 5)  // radius
+        .attr("r", 5)
         .style("fill", function(d) {
-            // The node color depends on the club.
             return color(d.sero_num); 
         });
-        // .call(force.drag);
 
-    // The name of each node is the node number.
     node.append("title")
         .text(function(d) { return d.p_id + " (" + d.sero + ")"; });
 
-    // We bind the positions of the SVG elements
-    // to the positions of the dynamic force-directed graph,
-    // at each time step.
     force.on("tick", function() {
-       // link.attr("x1", function(d) { return d.source.x; })
-       //     .attr("y1", function(d) { return d.source.y; })
-       //     .attr("x2", function(d) { return d.target.x; })
-       //     .attr("y2", function(d) { return d.target.y; });
-
         node.attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
     });
 });
+
 
 //
 // Tooltip
