2247b3e1e0b903890b3f00e55460d5a172cefad1
Piotr Migdal <pmigdal@gmail.com>
2015-05-29 15:53:37 +0200

    viewer testing

--- site/main.js
+++ site/main.js
@@ -205,7 +205,7 @@ function Legend (parentDom, node) {
         .style('fill', function (d) { return d.color; })
         .on('mouseover', function (d) {
           node.style('opacity', function (c) {
-            return c[field] == d.name ? 1 : 0.2;
+            return c[field] == d.name ? 1 : 0.1;
           })
         })
         .on('mouseout', function (d) {
@@ -221,7 +221,7 @@ function Legend (parentDom, node) {
         .text(function (d) { return d.name; })
         .on('mouseover', function (d) {
           node.style('opacity', function (c) {
-            return c[field] == d.name ? 1 : 0.2;
+            return c[field] == d.name ? 1 : 0.1;
           })
         })
         .on('mouseout', function (d) {
@@ -260,3 +260,44 @@ function Tooltip(parentDom) {
   };
 
 }
+
+//
+// protein viewer
+//
+var viewer = pv.Viewer(document.getElementById('structureViewer'), {
+  quality: 'medium',
+  width: 'auto',
+  height: 500, // 'auto',
+  antialias: true,
+  outline: true,
+  slabMode: 'auto'
+});
+
+function load(name, pdbPath) {
+  pv.io.fetchPdb(pdbPath, function(structure) {
+    // render everything as helix/sheet/coil cartoon, coloring by secondary 
+    // structure succession
+    var go = viewer.cartoon('structure', structure, {
+      color: pv.color.ssSuccession(),
+      showRelated: '1',
+    });
+
+    // find camera orientation such that the molecules biggest extents are 
+    // aligned to the screen plane.
+    var rotation = pv.viewpoint.principalAxes(go);
+    viewer.setRotation(rotation)
+
+    // adapt zoom level to contain the whole structure
+    viewer.autoZoom();
+  });
+}
+
+
+// load default
+load('example', 'ABK57092.pdb');
+
+// tell viewer to resize when window size changes.
+window.onresize = function(event) {
+  viewer.fitParent();
+}
+
