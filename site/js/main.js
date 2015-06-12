4ad7ed344ea938be322536f882fa64f3efcec67d
Piotr Migdal <pmigdal@gmail.com>
2015-06-12 19:48:25 +0200

    legend for superimposing

---
+++ site/js/main.js
@@ -0,0 +1,25 @@
+var proteinViewer = new ProteinViewer('structureViewer');
+
+// in future, data should be joined beforehand
+d3.json("data/graph.json", function(errorJSON, dataJSON) {
+  d3.csv("data/HA_metadata_pdb.csv", function(errorCSV, dataCSV) {
+
+    var additionalData = _.indexBy(dataCSV, 'id');
+    dataJSON.nodes = dataJSON.nodes
+      .map(function(d) {
+        return _.assign(d, additionalData[d.p_id]);
+      })
+      .map(function (d) {
+        return _.assign(d, {maintype: d.subtype ? d.subtype.split("N")[0] : undefined})
+      });
+
+
+
+    drawGraph(dataJSON);
+
+    // calculate h and n
+
+  });
+});
+
+
--- site/js/protein_viewer.js
+++ site/js/protein_viewer.js
@@ -4,7 +4,6 @@ function ProteinViewer(domId) {
 
   var colors = d3.scale.category10();
 
-  this.chainCounter = 0;
 
   var viewer = pv.Viewer(document.getElementById(domId), {
     quality: 'high',
@@ -14,6 +13,14 @@ function ProteinViewer(domId) {
     outline: true,
     slabMode: 'auto'
   });
+
+  this.colorNameList = [];
+
+  this.proteinLegend = new proteinLegend(domId);
+
+  //
+  // menu
+  //
 
   this.superimpose = true;
 
@@ -36,24 +43,77 @@ function ProteinViewer(domId) {
 
     pv.io.fetchPdb(pdbPath, function(structure) {
 
+      
       if (!that.superimpose) {
         viewer.clear();
-        that.chainCounter = 1;
-      } else {
-        that.chainCounter++;
+        that.colorNameList = [];
       }
 
-      console.log("that.chainCounter", that.chainCounter);
+      var color = colors(that.colorNameList.length);
+      that.colorNameList.push({name: name, color: color});
 
       var go = viewer.cartoon('structure', structure, {
-        color: pv.color.uniform(colors(that.chainCounter))
+        color: pv.color.uniform(color)
       });
 
       var rotation = pv.viewpoint.principalAxes(go);
       viewer.setRotation(rotation)
 
       viewer.autoZoom();
+
+      that.proteinLegend.update(that.colorNameList);
     });
+
   }
+
+}
+
+
+function proteinLegend (domId) {
+
+  this.svg = d3.select("#" + domId).append("svg")
+    .attr("class", "legendSvg")
+    .attr('width', 100)
+    .attr('height', 150);
+
+  this.update = function (colorNameList) {
+
+    console.log('colorNameList', colorNameList);
+
+    var boxes = this.svg.selectAll('.box')
+      .data(colorNameList);
+    var labels = this.svg.selectAll('.label')
+      .data(colorNameList);;
+
+    boxes.enter()
+      .append('rect')
+        .attr('class', 'box');
+    
+    boxes
+      .attr('x', 0)
+      .attr('y', function (d, i) { return 15 * i + 10; })
+      .attr('width', 10)
+      .attr('height', 10)
+      .style('fill', function (d) { return d.color; });
+
+    boxes.exit()
+      .remove();
+
+
+    labels
+      .enter()
+      .append('text')
+        .attr('class', 'label');
+
+    labels
+      .attr('x', 15)
+      .attr('y', function (d, i) { return 15 * i + 20; })
+      .text(function (d) { return d.name; });
+
+    labels.exit()
+      .remove();
+
+  };
+
 
 }
