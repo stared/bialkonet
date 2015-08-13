11933f0ed10f7691bc044017e5c14e842a914480
Piotr Migdal <pmigdal@gmail.com>
2015-08-13 19:50:08 +0200

    frontpage serograph

---
+++ site/js/index.js
@@ -0,0 +1,91 @@
+d3.csv("data/sero_distances.csv", function (error, data) {
+
+  var nodeNames = _.uniq(_.pluck(data, 'p_id').concat(_.pluck(data, 'p_id2')));
+
+  var nodes = nodeNames.map(function (x) { return {name: x}; });
+
+  var nodeId = _.zipObject(nodeNames.map(function (x, i) { return [x, i]; }));
+
+  var links = data.map(function (d) {
+    return {
+      source: nodeId[d.p_id],
+      target: nodeId[d.p_id2],
+      distance_mean: +d.distance_mean,
+      distance_median: +d.distance_median,
+      distance_std: +d.distance_std,
+    };
+  });
+
+  drawSeroGraph({nodes: nodes, links: links});
+
+});
+
+
+function drawSeroGraph(graph) {
+
+  var color = d3.scale.category20();
+
+  var width = 600,
+      height = 400;
+
+  var force = d3.layout.force()
+      .charge(-500)
+      .linkDistance(0)
+      .gravity(0.1)
+      .size([width - 150, height]);
+
+
+  var svg = d3.select("#serograph").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var tooltip = new Tooltip('#serograph');
+
+  force.nodes(graph.nodes)
+      .links(graph.links)
+      .linkStrength(function (d) {
+          return 0.1 * Math.min(Math.pow(d.distance_mean, -3), 1);
+      })
+      .start();
+
+  var drag = force.drag();
+
+  var link = svg.selectAll(".link")
+    .data(graph.links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("opacity", function (d) { return Math.min(Math.pow(d.distance_mean, -3), 1); })
+      .on('mouseover', function (d) {
+        tooltip.show([d.source.name, " and ", d.target.name, ":<br>mean distance: ", d.distance_mean.toFixed(2)].join(""));
+      })
+      .on('mouseout', function (d) {
+        tooltip.out();
+      });
+
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+      .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 20)
+        .style("fill", function (d, i) { return color(i); })
+        .on('mouseover', function (d) {
+          tooltip.show(d.name);
+        })
+        .on('mouseout', function (d) {
+          tooltip.out();
+        })
+        .call(drag);
+
+  force.on("tick", function() {
+
+      node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+
+      link.attr("x1", function(e) { return e.source.x; })
+          .attr("y1", function(e) { return e.source.y; })
+          .attr("x2", function(e) { return e.target.x; })
+          .attr("y2", function(e) { return e.target.y; });
+
+  });
+
+}
