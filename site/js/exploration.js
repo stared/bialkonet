92aecc1a1d7787b2fb15d4c9933f949db1906868
Piotr Migdal <pmigdal@gmail.com>
2015-09-17 19:44:13 +0200

    sequence viewer smaller

--- site/js/exploration.js
+++ site/js/exploration.js
@@ -2,7 +2,7 @@ var distanceGraph = new DistanceGraph('d3graph');
 
 var proteinViewer = new ProteinViewer('structureViewer');
 
-var sequenceViewer = new SequenceViewer("sequenceViewer", 500, 300, 20);
+var sequenceViewer = new SequenceViewer("sequenceViewer", 500, 100, 20);
 
 proteinViewer.onClickingAtom(sequenceViewer.blinkAt.bind(sequenceViewer));
 sequenceViewer.onZoom = proteinViewer.highlightFromTo;
