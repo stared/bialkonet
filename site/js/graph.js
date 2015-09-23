c90ba306816e98e1ac9f0b4bd18693ff32a947b0
Piotr Migdal <pmigdal@gmail.com>
2015-09-23 14:49:04 +0200

    a bit better esp distance functions

--- site/js/graph.js
+++ site/js/graph.js
@@ -183,10 +183,10 @@ function DistanceGraph(domId) {
 
     var wfs = {
       crystals_rmsd: function (d) { return Math.exp(-d.value/0.2); },
-      crystals_esp: function (d) {return Math.exp(-Math.sqrt(d.distance)/0.15); },
+      crystals_esp: function (d) {return Math.exp(-Math.pow(d.distance/0.15, 8)); },
       crystals_seq: function (d) {return Math.pow(d.distance/1800, 8); },
       models_rmsd: function (d) { return Math.exp(-d.value/0.5); },
-      models_esp: function (d) {return Math.exp(-Math.sqrt(d.distance)/0.15); },
+      models_esp: function (d) {return Math.exp(-Math.pow(d.distance/0.15, 8)); },
       models_seq: function (d) {return Math.pow(d.distance/1800, 8); },
     }
 
